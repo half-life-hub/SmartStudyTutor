e05c97367150a61358e59cc7221b1c18a933a8c9
Steven Marshall <pinkfloydim@me.com>
2025-10-20 09:26:34 +1030

    Initial Commit

---
+++ SmartStudyTutor/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SmartStudyTutor
+//
+//  Created by Steven Marshall on 20/10/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SmartStudyTutorTests/SmartStudyTutorTests.swift
@@ -0,0 +1,17 @@
+//
+//  SmartStudyTutorTests.swift
+//  SmartStudyTutorTests
+//
+//  Created by Steven Marshall on 20/10/2025.
+//
+
+import Testing
+@testable import SmartStudyTutor
+
+struct SmartStudyTutorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
